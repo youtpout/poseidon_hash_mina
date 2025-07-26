6115ae800333531b1a6ee13c632c5150375100d2
youtpout <eddyboughioul@hotmail.fr>
2025-07-26 17:29:55 +0200

    add a more complex benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -19,6 +19,44 @@ fn benchmark_poseidon_hashes(c: &mut Criterion) {
     group.finish();
 }
 
+fn benchmark_big_poseidon_hashes(c: &mut Criterion) {
+    let input = vec![
+        BigInt::from_str(
+            "7263514276861361464633452875109919113182485937660059416421780822303488364810",
+        )
+        .unwrap(),
+        BigInt::ZERO,
+        BigInt::from_str(
+            "14045753958617862754670070034440311287432747428158303518301078328357441472472",
+        )
+        .unwrap(),
+        BigInt::from(1u32),
+        BigInt::ZERO,
+        BigInt::ZERO,
+        BigInt::from(1000000000u32),
+        BigInt::from_str(
+            "23155254961521224263414982804952607540765699032359833230215492926554005931079",
+        )
+        .unwrap(),
+        BigInt::ZERO,
+        BigInt::from(8000000000u64),
+        BigInt::from(1000000000u32),
+        BigInt::ZERO,
+        BigInt::from(1u32),
+    ];
+
+    let mut group = c.benchmark_group("poseidon_group");
+
+    group.bench_function("big_poseidon_hash", |b| {
+        b.iter(|| {
+            let input_clone = black_box(input.clone());
+            let _ = PoseidonHash::hash(input_clone.clone());
+        });
+    });
+
+    group.finish();
+}
+
 fn bench_from_str(c: &mut Criterion) {
     let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
 
@@ -76,6 +114,7 @@ fn bench_biguint_from_bytes(c: &mut Criterion) {
 criterion_group!(
     benches,
     benchmark_poseidon_hashes,
+    benchmark_big_poseidon_hashes,
     bench_bigint_from_str,
     bench_bigint_from_bytes,
     bench_biguint_from_str,
--- tests/hash_test.rs
+++ tests/hash_test.rs
@@ -1,6 +1,6 @@
 use num_bigint::BigInt;
-use std::str::FromStr;
 use poseidon_hash_mina::PoseidonHash;
+use std::str::FromStr;
 
 #[cfg(test)]
 mod tests {
@@ -32,6 +32,41 @@ mod tests {
         let result = PoseidonHash::hash(input);
         let exepected: BigInt = BigInt::from_str(
             "24245350037390325723675562428846509781869515058976947458013661211417354108422",
+        )
+        .unwrap();
+        assert_eq!(result, exepected);
+        println!("Hash of [3412, 548748548]: {}", result);
+    }
+
+    #[test]
+    fn test_poseidon_hash_multiple_elements() {
+        let input = vec![
+            BigInt::from_str(
+                "7263514276861361464633452875109919113182485937660059416421780822303488364810",
+            )
+            .unwrap(),
+            BigInt::ZERO,
+            BigInt::from_str(
+                "14045753958617862754670070034440311287432747428158303518301078328357441472472",
+            )
+            .unwrap(),
+            BigInt::from(1u32),
+            BigInt::ZERO,
+            BigInt::ZERO,
+            BigInt::from(1000000000u32),
+            BigInt::from_str(
+                "23155254961521224263414982804952607540765699032359833230215492926554005931079",
+            )
+            .unwrap(),
+            BigInt::ZERO,
+            BigInt::from(8000000000u64),
+            BigInt::from(1000000000u32),
+            BigInt::ZERO,
+            BigInt::from(1u32),
+        ];
+        let result = PoseidonHash::hash(input);
+        let exepected: BigInt = BigInt::from_str(
+            "9477952037298579560464731632706228830146195233473478290570101327698566553181",
         )
         .unwrap();
         assert_eq!(result, exepected);
