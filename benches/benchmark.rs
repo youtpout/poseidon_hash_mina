d4b598e17065a3e910a1260ed29cce4100bced37
eddy <eddyboughioul@hotmail.fr>
2026-03-13 14:39:42 +0100

    update benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,123 +1,84 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
-use num_bigint::{BigInt, BigUint};
-use poseidon_hash_mina::PoseidonHash;
-use std::str::FromStr;
-// Import PoseidonHash from its module (update the path if needed)
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use poseidon_hash_mina::{Fp, Sponge};
 
-fn benchmark_poseidon_hashes(c: &mut Criterion) {
-    let input = vec![BigInt::from(3412), BigInt::from(548_748_548)];
-
-    let mut group = c.benchmark_group("poseidon_group");
-
-    group.bench_function("poseidon_hash", |b| {
-        b.iter(|| {
-            let input_clone = black_box(input.clone());
-            let _ = PoseidonHash::hash(input_clone.clone());
-        });
-    });
-
-    group.finish();
+fn fp(s: &str) -> Fp {
+    use num_bigint::BigInt;
+    use std::str::FromStr;
+    let p = BigInt::from_str(
+        "28948022309329048855892746252171976963363056481941560715954676764349967630337",
+    )
+    .unwrap();
+    let n = BigInt::from_str(s).unwrap();
+    let n = ((n % &p) + &p) % &p;
+    let (_, bytes) = n.to_bytes_be();
+    let mut arr = [0u8; 32];
+    arr[32 - bytes.len()..].copy_from_slice(&bytes);
+    Fp::from_be_bytes(arr)
 }
 
-fn benchmark_big_poseidon_hashes(c: &mut Criterion) {
-    let input = vec![
-        BigInt::from_str(
-            "7263514276861361464633452875109919113182485937660059416421780822303488364810",
-        )
-        .unwrap(),
-        BigInt::ZERO,
-        BigInt::from_str(
-            "14045753958617862754670070034440311287432747428158303518301078328357441472472",
-        )
-        .unwrap(),
-        BigInt::from(1u32),
-        BigInt::ZERO,
-        BigInt::ZERO,
-        BigInt::from(1000000000u32),
-        BigInt::from_str(
-            "23155254961521224263414982804952607540765699032359833230215492926554005931079",
-        )
-        .unwrap(),
-        BigInt::ZERO,
-        BigInt::from(8000000000u64),
-        BigInt::from(1000000000u32),
-        BigInt::ZERO,
-        BigInt::from(1u32),
+fn benchmark_poseidon_small(c: &mut Criterion) {
+    let inputs = [Fp::from(3412u64), Fp::from(548_748_548u64)];
+
+    c.bench_function("poseidon_hash_2_elements", |b| {
+        b.iter(|| Sponge::hash(black_box(&inputs)))
+    });
+}
+
+fn benchmark_poseidon_large(c: &mut Criterion) {
+    let inputs = [
+        fp("7263514276861361464633452875109919113182485937660059416421780822303488364810"),
+        Fp::ZERO,
+        fp("14045753958617862754670070034440311287432747428158303518301078328357441472472"),
+        Fp::from(1u64),
+        Fp::ZERO,
+        Fp::ZERO,
+        Fp::from(1_000_000_000u64),
+        fp("23155254961521224263414982804952607540765699032359833230215492926554005931079"),
+        Fp::ZERO,
+        Fp::from(8_000_000_000u64),
+        Fp::from(1_000_000_000u64),
+        Fp::ZERO,
+        Fp::from(1u64),
     ];
 
-    let mut group = c.benchmark_group("poseidon_group");
-
-    group.bench_function("big_poseidon_hash", |b| {
-        b.iter(|| {
-            let input_clone = black_box(input.clone());
-            let _ = PoseidonHash::hash(input_clone.clone());
-        });
-    });
-
-    group.finish();
-}
-
-fn bench_from_str(c: &mut Criterion) {
-    let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
-
-    c.bench_function("BigInt::from_str", |b| {
-        b.iter(|| {
-            let _ = BigInt::from_str(val_str).unwrap();
-        })
+    c.bench_function("poseidon_hash_13_elements", |b| {
+        b.iter(|| Sponge::hash(black_box(&inputs)))
     });
 }
 
-fn bench_bigint_from_str(c: &mut Criterion) {
-    let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
+fn benchmark_fp_mul(c: &mut Criterion) {
+    let a = fp("7263514276861361464633452875109919113182485937660059416421780822303488364810");
+    let b = fp("14045753958617862754670070034440311287432747428158303518301078328357441472472");
 
-    c.bench_function("BigInt::from_str", |b| {
-        b.iter(|| {
-            let _ = BigInt::from_str(val_str).unwrap();
-        })
+    c.bench_function("fp_mul", |b_| {
+        b_.iter(|| black_box(a).mul(black_box(b)))
     });
 }
 
-fn bench_bigint_from_bytes(c: &mut Criterion) {
-    let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
-    let bigint = BigInt::from_str(val_str).unwrap();
-    let bytes = bigint.to_signed_bytes_be();
+fn benchmark_fp_pow7(c: &mut Criterion) {
+    let a = fp("7263514276861361464633452875109919113182485937660059416421780822303488364810");
 
-    c.bench_function("BigInt::from_signed_bytes_be", |b| {
-        b.iter(|| {
-            let _ = BigInt::from_signed_bytes_be(&bytes);
-        })
+    c.bench_function("fp_pow7", |b| {
+        b.iter(|| black_box(a).pow7())
     });
 }
 
-fn bench_biguint_from_str(c: &mut Criterion) {
-    let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
-
-    c.bench_function("BigUint::from_str", |b| {
+fn benchmark_permute(c: &mut Criterion) {
+    c.bench_function("poseidon_permute", |b| {
         b.iter(|| {
-            let _ = BigUint::from_str(val_str).unwrap();
-        })
-    });
-}
-
-fn bench_biguint_from_bytes(c: &mut Criterion) {
-    let val_str = "12035446894107573964500871153637039653510326950134440362813193268448863222019";
-    let biguint = BigUint::from_str(val_str).unwrap();
-    let bytes = biguint.to_bytes_be();
-
-    c.bench_function("BigUint::from_bytes_be", |b| {
-        b.iter(|| {
-            let _ = BigUint::from_bytes_be(&bytes);
+            let mut s = Sponge::new();
+            s.permute();
+            black_box(s.squeeze())
         })
     });
 }
 
 criterion_group!(
     benches,
-    benchmark_poseidon_hashes,
-    benchmark_big_poseidon_hashes,
-    bench_bigint_from_str,
-    bench_bigint_from_bytes,
-    bench_biguint_from_str,
-    bench_biguint_from_bytes
+    benchmark_poseidon_small,
+    benchmark_poseidon_large,
+    benchmark_fp_mul,
+    benchmark_fp_pow7,
+    benchmark_permute,
 );
 criterion_main!(benches);
